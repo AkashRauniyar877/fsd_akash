6a6938c51971c441333c717c3951bcbc66458085
AkashRauniyar877 <akashrauniyar877@gmail.com>
2025-12-02 13:01:55 +0530

    ..

---
+++ WEBDEVELOPMENT/HTML/december/1st dec/2nd dec/script.js
@@ -0,0 +1,57 @@
+// var obj=null;
+// console.log(obj);
+// console.log(typeof(obj));
+// console.log(null==undefined)
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+// console.log(NaN/2);
+// console.log(typeof(NaN));
+// console.log(isNaN(NaN));
+// console.log(isNaN("hello"));
+// console.log(isNaN(123));
+// s='Hello, what\'s up "How are you?" ';
+// console.log(s);
+ let n=200.05;
+console.log(typeof(n));
+
+// let amount= 129_224.99;
+// console.log(amount);
+// let msg=`"it's a sunny day" 
+// Let's make the most of it.`; //template literal
+// console.log(msg);
+
+// let name = 'John';
+// let msg = 'Hi, i am $(name)';
+// console.log(msg);
+
+// let msg2 = `Hi, I am ${name}`;
+// console.log(msg2);
+
+// s='Hello';
+// console.log(s.length);
+// console.log(s[0]);
+// console.log(s[s.length-1]);
+
+
+//  let status =false;
+//  console.log(status);
+//  let str = status.toString();
+//  console.log(str);
+//  let back = Boolean(str);
+//  console.log(back);
+//  console.log(typeof(back));
+
+let person = {
+    fname: 'John',
+    lname: 'Doe',
+    age: 25
+}
+console.log(person);
+console.log(typeof(person));
+console.log(person.fname);
+console.log(person['lname']);
+person.age=26;
+console.log(person.age);
+person.city='New York';
+console.log(person);
